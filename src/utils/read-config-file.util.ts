217fadc1bb147a0e6c0290cc09298ca78cad836c
Chun-Kai Wang <chunkai1312@gmail.com>
2022-03-11 14:43:43 +0800

    refactor: read config file

--- src/utils/read-config-file.util.ts
+++ src/utils/read-config-file.util.ts
@@ -6,10 +6,10 @@ export function readConfigFile(path: string): ClientConfig {
   const config = ini.parse(fs.readFileSync(path, 'utf-8'));
 
   return {
-    apiUrl: config?.Core?.Entry,
-    apiKey:config?.Api?.Key,
-    apiSecret: config?.Api?.Secret,
-    certPath: config?.Cert?.Path,
-    aid: config?.User?.Account,
+    apiUrl: config && config.Core && config.Core.Entry,
+    apiKey: config && config.Api && config.Api.Key,
+    apiSecret: config && config.Api && config.Api.Secret,
+    certPath: config && config.Cert && config.Cert.Path,
+    aid: config && config.User && config.User.Account,
   };
 }
